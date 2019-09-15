2fe1f11e177f5dffc4609f429fc61d9e205922a7
Amit <amitzmn@gmail.com>
2019-09-15 19:33:27 -0400

    Reverted to ES5 function and also utilized the Person Component INSIDE the new Group Component

--- src/index.js
+++ src/index.js
@@ -2,17 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-// function Person(props) {
-//   return <h1>Hi {props.name}</h1>
-// }
-
-class Person extends React.Component {
-  render() {
-    return <h1>Hi {this.props.name}</h1>
-  }
+function Person(props) {
+  return <h1>Hi {props.name} is with the {props.group}</h1>
 }
 
+function Group() {
+  return <div>
+    <Person name='Amit' group='Frontend Devs' />
+    <Person name='Christina' group='Backend Devs' />
+  </div>
+}
+
+// class Person extends React.Component {
+//   render() {
+//     return <h1>Hi {this.props.name}</h1>
+//   }
+// }
+
+
+
   ReactDOM.render(
-    <Person name='Amit'/>,
+    <Group />,
       document.getElementById('root')
   );
