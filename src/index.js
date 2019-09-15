47c13f1c55c4e0f20f649a16a31db4b20bc02d3c
Amit <amitzmn@gmail.com>
2019-09-15 19:16:30 -0400

    created the same Person Component but using ES6's Class to define the component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-function Person(props) {
-  return <h1>Hi {props.name}</h1>
+// function Person(props) {
+//   return <h1>Hi {props.name}</h1>
+// }
+
+class Person extends React.Component {
+  render() {
+    return <h1>Hi {this.props.name}</h1>
+  }
 }
 
   ReactDOM.render(
