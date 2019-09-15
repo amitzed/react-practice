bc176b6496211e8115c18dc2e26f81af37c3d0da
Amit <amitzmn@gmail.com>
2019-09-15 18:51:12 -0400

    created function for clock in index.js to reflect local time in browser

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+let clock = () => {
+  return <h2>
+    {new Date().toLocaleTimeString()}
+  </h2>
+};
+
+ReactDOM.render(
+  clock(),
+    document.getElementById('root')
+);
