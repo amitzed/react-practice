f385c40dae1fadf9b6da7aa09c36a618866df13a
Amit <amitzmn@gmail.com>
2019-09-15 18:55:29 -0400

    Cut entire <h2> element  and put it in ReactDOM -- THEN wrapped the whole ReactDOM code in setInterval function: Now clock time is updating

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 let clock = () => {
-  return <h2>
-    {new Date().toLocaleTimeString()}
-  </h2>
+  return
 };
 
-ReactDOM.render(
-  clock(),
-    document.getElementById('root')
-);
+setInterval(function() {
+  ReactDOM.render(
+    <h2>
+      {new Date().toLocaleTimeString()}
+    </h2>,
+      document.getElementById('root')
+  );
+});
